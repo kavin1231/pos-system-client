f367f0f6e577a728c0cfe5291cb28fb25ee027cf
infofuturebound-afk <info.futurebound@gmail.com>
2025-10-19 21:22:02 +0530

    2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {LoadingComponent} from './components/loading/loading.component';
+import {LoadingStatusService} from './services/loading-status.service';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoadingComponent, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'pos-system-client';
+  public statusService:LoadingStatusService = inject(LoadingStatusService);
 }
---
+++ src/app/interceptor/http-manager.interceptor.ts
@@ -0,0 +1,6 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+
+export const httpManagerInterceptor: HttpInterceptorFn = (req, next) => {
+
+  return next(req)
+};
